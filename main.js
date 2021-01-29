98f2910a9d35730560b83be2da82446be11dc139
Diana Patricia Bautista Otalora <diana.bautista.otalora@gmail.com>
2021-01-28 20:52:48 -0500

    for

---
+++ main.js
@@ -0,0 +1,26 @@
+// console.time("Tiempo en ejecución");
+// for ( let i =0; i <10; i++){
+//     console.log(i);
+// }
+// console.timeEnd("for()");
+
+// console.time("for()");
+// let lista = ["Miguel", "Jhon", "Diana", "Adrian", "Diego", "Julian"];
+// for ( let i =0; i < lista.length; i++){
+//     console.log(i);
+//     console.log(lista[i]);
+// }
+// console.timeEnd("for()");
+
+// console.time("for()");
+// let lista = ["Miguel", "Jhon", "Diana", "Adrian", "Diego", "Julian"];
+// for (let nombre of lista){
+//     console.log(nombre);
+// }
+// console.timeEnd("for()");
+console.time("for()");
+let lista = ["Miguel", "Jhon", "Diana", "Adrian", "Diego", "Julian"];
+for (let nombre of lista){
+    console.log(nombre);
+}
+console.timeEnd("for()");
